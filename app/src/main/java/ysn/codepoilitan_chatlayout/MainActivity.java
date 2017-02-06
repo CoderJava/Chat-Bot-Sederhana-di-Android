5652d9b8d01f92d5c8f956c5524d00bce742c520
CoderJava <kolonel.yudisetiawan@gmail.com>
2017-02-06 14:05:51 +0700

    ChatBot Sederhana di Android Versi 1.1.0

--- app/src/main/java/ysn/codepoilitan_chatlayout/MainActivity.java
+++ app/src/main/java/ysn/codepoilitan_chatlayout/MainActivity.java
@@ -1,5 +1,7 @@
 package ysn.codepoilitan_chatlayout;
 
+import android.content.res.AssetManager;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -7,9 +9,21 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import org.alicebot.ab.AIMLProcessor;
+import org.alicebot.ab.Bot;
+import org.alicebot.ab.Chat;
+import org.alicebot.ab.MagicStrings;
+import org.alicebot.ab.PCAIMLProcessorExtension;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -20,6 +34,7 @@ import ysn.codepoilitan_chatlayout.model.DataSms;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = "MainActivityTAG";
     private RecyclerView recyclerViewMain;
     private EditText editTextIsiPesan;
     private FloatingActionButton floatingActionButtonKirimPesan;
@@ -28,6 +43,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     List<DataSms> listDataSms;
     AdapterSms adapterSms;
     SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss");
+
+    private Bot bot;
+    private Chat chat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +62,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void loadData() {
+        //  checking SD Card availability
+        boolean sdCardAvailable = isSdCardAvailable();
+
+        //  receiving the assets from the app directory
+        AssetManager assetManager = getResources().getAssets();
+        File dir = new File(Environment.getExternalStorageDirectory().toString() + "/chatbot/bots/Popoyo");
+        boolean makeDir = dir.mkdirs();
+        if (dir.exists()) {
+            //  reading the file assets
+            try {
+                for (String fileAsset : assetManager.list("chatbot")) {
+                    File subdir = new File(dir.getPath() + "/" + fileAsset);
+                    boolean subdirCheck = subdir.mkdirs();
+                    for (String file : assetManager.list("chatbot/" + fileAsset)) {
+                        File f = new File(dir.getPath() + "/" + fileAsset + "/" + file);
+                        if (f.exists()) {
+                            continue;
+                        }
+                        InputStream in = null;
+                        OutputStream out = null;
+                        in = assetManager.open("chatbot/" + fileAsset + "/" + file);
+                        out = new FileOutputStream(dir.getPath() + "/" + fileAsset + "/" + file);
+
+                        //  copy file from assets to the mobile SD card or any secondary memory
+                        copyFile(in, out);
+                        in.close();
+                        in = null;
+                        out.flush();
+                        out.close();
+                        out = null;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        //  get the working directory
+        MagicStrings.root_path = Environment.getExternalStorageDirectory().toString() + "/chatbot";
+        System.out.println("working directory: " + MagicStrings.root_path);
+        AIMLProcessor.extension = new PCAIMLProcessorExtension();
+
+        //  assign the AIML files to bot for processing
+        bot = new Bot("Popoyo", MagicStrings.root_path, "chat");
+        chat = new Chat(bot);
+
+
         listViewType = new ArrayList<>();
         listViewType.add(AdapterSms.VIEW_TYPE_KIRI);
 
@@ -58,6 +123,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerViewMain.setLayoutManager(layoutManager);
         recyclerViewMain.setAdapter(adapterSms);
+    }
+
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while ((read = in.read(buffer)) != -1) {
+            out.write(buffer, 0, read);
+        }
+    }
+
+    private boolean isSdCardAvailable() {
+        return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED) ? true : false;
     }
 
 
@@ -76,6 +153,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 dataSms.setPesan(pesan);
                 dataSms.setWaktu(simpleDateFormat.format(new Date()));
                 listDataSms.add(dataSms);
+
+                //  bot response
+                String botResponse = chat.multisentenceRespond(pesan);
+                listViewType.add(AdapterSms.VIEW_TYPE_KIRI);
+                DataSms dataSmsBot = new DataSms();
+                dataSmsBot.setPesan(botResponse);
+                dataSmsBot.setWaktu(simpleDateFormat.format(new Date()));
+                listDataSms.add(dataSmsBot);
                 adapterSms.notifyDataSetChanged();
             }
         }
